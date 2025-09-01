07ce004d570dff90573cf2daf10c8122b455ce75
Mayara de Oliveira Sant'Anna <159502755+mayarasantanna2@users.noreply.github.com>
2025-09-01 09:08:38 -0300

    Add files via upload

---
+++ biblioteca0109/biblioteca.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 01-Set-2025 às 14:06
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca`
+--
+CREATE DATABASE IF NOT EXISTS `biblioteca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `biblioteca`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id` int(11) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `genero` varchar(255) NOT NULL,
+  `ano` int(4) NOT NULL,
+  `paginas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`id`, `autor`, `titulo`, `genero`, `ano`, `paginas`) VALUES
+(1, ' Matt Haig', 'A Biblioteca da Meia-Noite', ' Ficção Científica, Ficção Filosófica, Literatura Fantástica', 2020, 308),
+(2, ' Neil Gaiman', ' Coraline', '  Literatura infantil, Conto de fadas, Terror, Literatura fantástica, Ficção paranormal', 2002, 125),
+(3, ' Colleen Hoover', 'É assim que acaba', 'Romance de amor, Ficção, Romance contemporâneo', 2016, 368),
+(4, 'Colleen Hoover', 'É assim que começa', 'Romance de amor, Romance contemporâneo, Ficção de novo adulto', 2022, 336),
+(5, 'Larissa Manoela', 'O diário de Larissa Manoela: A vida, a história e os segredos da jovem estrela', 'Autobiografia', 2016, 165);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ biblioteca0109/pedaco.php
@@ -0,0 +1,2 @@
+<h1>BIBLIOTECA DA ETEC MCM</h1>
+    <h2>Aluna Mayara de Oliveira  - Aula de SW1 (Prof. Anderson)</h2>
